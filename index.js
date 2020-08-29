d1f0f8629c087082efcbab70aec114c1eba6848b
Ayush <Ayush-15@live.com>
2020-08-29 18:11:11 +0530

    .

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello " + req.query.name ? req.query.name : " ");
+});
+
+app.get("/users", function (req, res) {
+  res.send({ name: "AYush", Age: "21", Gender: "Male" });
+});
+
+app.listen(3000);
+console.log("Server is running at port 3000");
